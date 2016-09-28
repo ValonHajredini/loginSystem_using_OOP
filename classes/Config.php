f73da9a517610a269b500b6f055f6617b125bc30
Valon Hajredini <hajredini.valon@gmail.com>
2016-09-28 14:12:01 +0200

    Returning the Config array usint Class Config and static method get()

--- classes/Config.php
+++ classes/Config.php
@@ -7,5 +7,18 @@
  * Time: 1:29 PM
  */
 class Config{
-
+    public static function get($path = null){
+        if($path){
+            $config = $GLOBALS['config'];
+            $path = explode('/', $path);
+            foreach($path as $path_index){
+                if(isset($config[$path_index])){
+                    $config = $config[$path_index];
+                }
+            }
+            return $config;
+        } else {
+            return false;
+        }
+    }
 }
--- index.php
+++ index.php
@@ -5,3 +5,5 @@
  * Date: 9/28/2016
  * Time: 1:26 PM
  */
+require_once 'core/init.php';
+echo Config::get('mysql/host');
