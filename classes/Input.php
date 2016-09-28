c720f86f697d599d3c6ab4516cd87e8035e4d5c7
Valon Hajredini <hajredini.valon@gmail.com>
2016-09-29 01:00:31 +0200

    Added Input class for geting the params

--- classes/Input.php
+++ classes/Input.php
@@ -6,7 +6,38 @@
  * Date: 9/28/2016
  * Time: 1:30 PM
  */
-class Input
-{
+class Input{
+    public static function exists($type = 'post'){
+        switch ($type){
+            case 'post':
+                return (!empty($_POST)) ? true : false;
+                break;
 
+            case 'get';
+                return (!empty($_GET)) ? true : false;
+                break;
+            default:
+                return false;
+            break;
+        }
+    }
+    public static function get($item){
+        if(isset($_POST[$item])){
+            return $_POST[$item];
+        } else if (isset($_GET[$item])) {
+            return $_GET[$item];
+        }else {
+            return '';
+        }
+    }
+    public static function post($item){
+        return $_POST[$item];
+    }
+    public static function param($item){
+        if(isset($_POST[$item])){
+            return $_POST[$item];
+        } else {
+            return $_GET[$item];
+        }
+    }
 }
--- register.php
+++ register.php
@@ -1,7 +1,34 @@
-<?php
+<?php require_once 'core/init.php';
 /**
  * Created by PhpStorm.
  * User: hajre
  * Date: 9/28/2016
  * Time: 1:27 PM
  */
+if (Input::exists()){
+//    echo Input::post('username');
+    echo Input::get('name');
+}
+?>
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="password">Chose Password</label>
+        <input type="password" name="password" id="password" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="password_again">Psssword Again</label>
+        <input type="password" name="password_again" id="password_again" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="name">name</label>
+        <input type="text" name="name" id="name" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <input type="submit" value="Register">
+    </div>
+
+</form>
