ffbbf01cc65f2eda7d955c50f1d2b366838e1446
Valon Hajredini <hajredini.valon@gmail.com>
2016-09-29 14:48:30 +0200

    Completed the registration functionality

--- classes/Hash.php
+++ classes/Hash.php
@@ -6,7 +6,15 @@
  * Date: 9/28/2016
  * Time: 1:30 PM
  */
-class Hash
-{
+class Hash{
+    public static function make($string, $salt = ''){
+        return hash('sha256', $string.$salt);
+    }
+    public static function salt($length){
+        return mcrypt_create_iv($length);
+    }
+    public static function unique(){
+        return self::make(uniqid());
+    }
 
 }
--- classes/User.php
+++ classes/User.php
@@ -6,7 +6,16 @@
  * Date: 9/28/2016
  * Time: 1:32 PM
  */
-class User
-{
+class User{
+    private $_db;
+
+    public function __construct($user = null){
+        $this->_db = DB::getInstance();
+    }
+    public function create($fields = array()){
+        if (!$this->_db->insert('users', $fields)){
+            throw new Exception('There wos a problem creating an acount');
+        }
+    }
 
 }
